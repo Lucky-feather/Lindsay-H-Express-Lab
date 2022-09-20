05e5813795b0181db00ec17c448bc4ea75909f46
LH <lindsaynhenderson@gmail.com>
2022-09-19 20:11:58 -0500

    setting up home

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', function(req, res) {
   res.send('<h1> hello, friends </h1> ')
 })
 
+app.get('/home', function(req, res) {
+  res.render('home')
+})
+
 app.listen(3000, function() {
   console.log('listening on port 3000')
 })
