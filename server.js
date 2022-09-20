f945f01721348a49415996ed8fc81648b7c5f6ef
LH <lindsaynhenderson@gmail.com>
2022-09-19 19:59:09 -0500

    pushing before trying killall

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', function(req, res) {
+  res.send('<h1> hello, friends </h1> ')
+})
+
+app.listen(3000, function() {
+  console.log('listening on port 3000')
+})
+
