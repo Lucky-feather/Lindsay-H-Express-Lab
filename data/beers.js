deb54778691e4805e7b931e9559b07088a7d90d2
LH <lindsaynhenderson@gmail.com>
2022-09-19 23:08:14 -0500

    complete basic lab

---
+++ data/beers.js
@@ -0,0 +1,10 @@
+const beers = [
+  {text: "Brown Dog Ale", sour: false,  price: "6",  _id: 125223},
+  {text: "Snazzy Sour", sour: true, price: "7", _id: 127904},
+  {text: "Beginner's Luck IPA", sour: false, price: "6", _id: 139608},
+  {text: "Really Dark Beer", sour: false, price: "8", _id: 139609},
+]
+
+export { 
+	beers
+}
--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 import express from 'express'
-import { beers } from './data/beer-data.js'
+import { beers } from './data/beers.js'
 
 const app = express()
 
-// app.get('/', function(req, res) {
-//   res.send('<h1> hello, friends </h1> ')
-// })
 
-app.get('/home', function(req, res) {
-  res.render("home")
+app.get('/', function(req, res) {
+  res.render('/beers')
 })
 
-app.get("/beers", function(req, res) {
-  res.render("beers.index", {
-    beers:beers
+app.get('/beers', function(req, res) {
+  res.render('beers/index', {
+    beers: beers
   })
 })
 
-app.listen(3000, function() {
-  console.log("listening on port 3000")
+// Configure the app (app.set)
+app.set('view engine', 'ejs')
+
+app.listen(3030, function() {
+  console.log("listening on port 3030")
 })
 
