a114b6615710b376da68dac378e111c57c3d6655
LH <lindsaynhenderson@gmail.com>
2022-09-19 21:21:31 -0500

    add beer-data.js

---
+++ data/beer-data.js
@@ -0,0 +1,9 @@
+const beers = [
+  {text: "Brown Dog Ale", sour: false, _id: 125223},
+  {text: "Snazzy Sour", sour: true, _id: 127904},
+  {text: "Beginner's Luck IPA", sour: false, _id: 139608},
+]
+
+export { 
+	beers
+}
--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 import express from 'express'
+import { beers } from './data/beer-data.js'
 
 const app = express()
 
-app.get('/', function(req, res) {
-  res.send('<h1> hello, friends </h1> ')
-})
+// app.get('/', function(req, res) {
+//   res.send('<h1> hello, friends </h1> ')
+// })
 
 app.get('/home', function(req, res) {
-  res.render('home')
+  res.render("home")
+})
+
+app.get("/beers", function(req, res) {
+  res.render("beers.index", {
+    beers:beers
+  })
 })
 
 app.listen(3000, function() {
-  console.log('listening on port 3000')
+  console.log("listening on port 3000")
 })
 
